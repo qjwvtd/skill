9e25bbb56604e6085ba81b0f6148e723cf2c7902
qjwvtd <176156562@qq.com>
2018-08-20 16:23:40 +0800

    first

--- web/public/session.js
+++ web/public/session.js
@@ -4,31 +4,46 @@
  * 使用cookie封装的cookie类
  * **/
 //Storage(支持现代浏览器)
-export class Storage{
-    setItem(key,value){
-        sessionStorage.setItem(key,JSON.stringify(value));
+export class Storage {
+    setItem(key, value) {
+        sessionStorage.setItem(key, JSON.stringify(value));
     }
-    getItem(key){
+
+    getItem(key) {
         return JSON.parse(sessionStorage.getItem(key));
     }
-    removeItem(key){
+
+    removeItem(key) {
         sessionStorage.removeItem(key);
     }
 }
 //cookie(本地支持IE,本地高级浏览器不支持，布署后服务器支持)
-export class Cookie{
-    setCookie(name,value){
-        document.cookie = name + "=" + value;
+export class Cookie {
+    setCookie(name, value, delay) {
+        /**
+         * delay:cookie有效期限,默认1天，天数，如：1
+         **/
+        const Days = delay ? delay : 1; //默认保存1天
+        const exp = new Date();
+        exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+        document.cookie = name + "=" + value + ";expires=" + exp.toGMTString();
     }
-    getCookie(name){
-        const cName = name + "=";
-        const ca = document.cookie.split(';');
-        for(let i=0; i<ca.length; i++){
-            const c = ca[i].trim();
-            if(c.indexOf(cName) == 0) {
-                return c.substring(cName.length,c.length);
-            };
+
+    getCookie(name) {
+        const arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+        if (arr != null) {
+            return arr[2];
+        } else {
+            return null;
         }
-        return "";
+    }
+
+    delCookie(name) {
+        var exp = new Date();
+        exp.setTime(exp.getTime() - 1);
+        var cval = this.getCookie(name);
+        if (cval != null) {
+            document.cookie = name + "=" + cval + ";expires=" + exp.toGMTString();
+        }
     }
 }
