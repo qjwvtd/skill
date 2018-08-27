5bba837b21e2bb2c25b1c72f2def395265e66fe4
qjwvtd <176156562@qq.com>
2018-08-27 18:11:59 +0800

    first

--- web/src/component/testSelect.js
+++ web/src/component/testSelect.js
@@ -12,11 +12,13 @@ export default class SelectTest extends Component {
 
     render() {
         return (
-            <select name="" id="" style={{width:'200px',height:'30px'}}>
-                <option value="">1</option>
-                <option value="">2</option>
-                <option value="">3</option>
-            </select>
+            <div style={{width:'200px',height:'30px',border:'1px solid red'}}>
+                <select name="" id="" style={{width:'200px',height:'30px',opacity:'0'}}>
+                    <option value="">1</option>
+                    <option value="">2</option>
+                    <option value="">3</option>
+                </select>
+            </div>
         );
     }
 }
