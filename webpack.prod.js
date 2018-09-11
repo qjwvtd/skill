9f0239c0bb1d899ec3e0a8e6c7bf29203230933f
qjwvtd <176156562@qq.com>
2018-09-11 13:46:15 +0800

    2018-08-31 制作日期插件...

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,14 +5,25 @@ const baseConfig = require('./webpack.config.js');
 //const HtmlWebpackPlugin = require('html-webpack-plugin');
 //baseConfig.entry.vendor = ['react','react-dom'];
 //baseConfig.optimization = {
+//    minimize:true,
+//    runtimeChunk: {
+//        name: "manifest"
+//    },
 //    splitChunks: {
-//        chunks: "all",
-//            minChunks: 1,
-//            minSize: 0,
-//            cacheGroups: {
+//        cacheGroups: {
+//            common: {
+//                chunks: "all",
+//                    name: "common",
+//                    minChunks: 2,
+//                    maxInitialRequests: 5,
+//                    minSize: 0
+//            },
 //            vendor: {
-//                test: "vendor",
-//                name: "vendor"
+//                test: /node_modules/,
+//                    chunks: "all",
+//                    name: "vendor",
+//                    priority: 10,
+//                    enforce: true
 //            }
 //        }
 //    }
