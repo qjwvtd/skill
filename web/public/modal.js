69c96072a3854c02ce63a18b1f875793d1d4ac4b
qjwvtd <176156562@qq.com>
2018-09-25 13:39:42 +0800

    2018-08-31 制作日期插件...

--- web/public/modal.js
+++ web/public/modal.js
@@ -26,121 +26,29 @@ class ModalHead extends Component{
         );
     }
 }
-
-
-//类似于alert(只有一个确认按钮)
-export class ModalAlert extends Component{
+export default class Modal extends Component{
     constructor(props){
         super(props);
         this.body = window.document.body;
+        this.state = {
+            title:props.title ? props.title : '标题',
+            width:props.width ? props.width : '600px'
+        };
     }
     closeModal(bool){
         this.props.close(bool);
     }
     render(){
         const { children } = this.props;
-        const title = this.props.title ? this.props.title : '标题';
-        const width = this.props.width ? this.props.width : '600px';
-        const isActiveClassBody = this.props.status ? 'ui-modal-body active' : 'ui-modal-body';
-        const isActiveWidth = this.props.status ? width : '0';
         this.node = <div className={this.props.status ? 'ui-modal active' : 'ui-modal'}>
-            <div className={isActiveClassBody} style={{width:isActiveWidth}}>
-                <ModalHead title={title} close={this.closeModal.bind(this)} />
-                <div className="ui-modal-content">
-                    { children }
-                </div>
-                <div className="ui-modal-foot">
-                    <span className="btnOK" onClick={this.closeModal.bind(this,true)}>确定</span>
-                </div>
-            </div>
-        </div>;
-        return this.props.status ? createPortal(this.node,this.body) : null;
-    }
-}
-//类似于confirm(两个按钮)
-export class ModalConfirm extends Component{
-    constructor(props){
-        super(props);
-        this.body = window.document.body;
-    }
-    closeModal(bool){
-        this.props.close(bool);
-    }
-    render(){
-        const { children } = this.props;
-        const title = this.props.title ? this.props.title : '标题';
-        const width = this.props.width ? this.props.width : '600px';
-        const isActiveClassBody = this.props.status ? 'ui-modal-body active' : 'ui-modal-body';
-        const isActiveWidth = this.props.status ? width : '0';
-        this.node = <div className={this.props.status ? 'ui-modal active' : 'ui-modal'}>
-            <div className={isActiveClassBody} style={{width:isActiveWidth}}>
-                <ModalHead title={title} close={this.closeModal.bind(this)} />
+            <div className="ui-modal-body" style={{width:this.state.width}}>
+                <ModalHead title={this.state.title} close={this.closeModal.bind(this)} />
                 <div className="ui-modal-content">
                     { children }
                 </div>
                 <div className="ui-modal-foot">
                     <span className="btnNO" onClick={this.closeModal.bind(this,false)}>取消</span>
                     <span className="btnOK" onClick={this.closeModal.bind(this,true)}>确定</span>
-                </div>
-            </div>
-        </div>;
-        return this.props.status ? createPortal(this.node,this.body) : null;
-    }
-}
-//类似于prompt(弹出一个输入框)
-export class ModalPrompt extends Component{
-    constructor(props){
-        super(props);
-        this.body = window.document.body;
-    }
-    closeModal(bool){
-        const value = this.refs.promptModalIn.value;
-        if(bool && value.length > 0){
-            this.props.close(bool,value);
-        }else{
-            this.props.close();
-        }
-
-    }
-    render(){
-        const title = this.props.title ? this.props.title : '标题';
-        const width = this.props.width ? this.props.width : '600px';
-        const isActiveClassBody = this.props.status ? 'ui-modal-body active' : 'ui-modal-body';
-        const isActiveWidth = this.props.status ? width : '0';
-        this.node = <div className={this.props.status ? 'ui-modal active' : 'ui-modal'}>
-            <div className={isActiveClassBody} style={{width:isActiveWidth}}>
-                <ModalHead title={title} close={this.closeModal.bind(this)} />
-                <div className="ui-modal-content">
-                    <input type="text" ref="promptModalIn" className="promptModalIn" placeholder="Please enter text" />
-                </div>
-                <div className="ui-modal-foot">
-                    <span className="btnNO" onClick={this.closeModal.bind(this,false)}>取消</span>
-                    <span className="btnOK" onClick={this.closeModal.bind(this,true)}>确定</span>
-                </div>
-            </div>
-        </div>;
-        return this.props.status ? createPortal(this.node,this.body) : null;
-    }
-}
-//无确认按钮、取消按钮的会话框
-export class ModalDilog extends Component{
-    constructor(props){
-        super(props);
-        this.body = window.document.body;
-    }
-    closeModal(bool){
-        this.props.close(bool);
-    }
-    render(){
-        const title = this.props.title ? this.props.title : '标题';
-        const width = this.props.width ? this.props.width : '600px';
-        const isActiveClassBody = this.props.status ? 'ui-modal-body active' : 'ui-modal-body';
-        const isActiveWidth = this.props.status ? width : '0';
-        this.node = <div className={this.props.status ? 'ui-modal active' : 'ui-modal'}>
-            <div className={isActiveClassBody} style={{width:isActiveWidth}}>
-                <ModalHead title={title} close={this.closeModal.bind(this)} />
-                <div className="ui-modal-content">
-                    {this.props.message}
                 </div>
             </div>
         </div>;
--- web/src/component/testModal.js
+++ web/src/component/testModal.js
@@ -2,81 +2,27 @@
  * 此处是注释
  **/
 import React,{Component} from 'react';
-import {ModalAlert,ModalConfirm,ModalPrompt,ModalDilog} from './../../public/modal';
+import Modal from './../../public/modal';
 export default class ModalTest extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            alertStatus:false,
-            confirmStatus:false,
-            promptStatus:false,
-            dilogStatus:false
+            status:false
         };
     };
-    //alert
-    openAlert(){
-        const as = this.state.alertStatus;
+    openModal(){
+        const as = this.state.status;
         switch (as){
         case true:
-            this.setState({alertStatus:false});
+            this.setState({status:false});
             break;
         case false:
-            this.setState({alertStatus:true});
+            this.setState({status:true});
             break;
         }
     }
-    closeAlert(bool){
-        this.setState({alertStatus:false});
-        console.log(bool);
-    }
-    //confirm
-    openConfirm(){
-        const as = this.state.confirmStatus;
-        switch (as){
-        case true:
-            this.setState({confirmStatus:false});
-            break;
-        case false:
-            this.setState({confirmStatus:true});
-            break;
-        }
-    }
-    closeConfirm(bool){
-        this.setState({confirmStatus:false});
-        console.log(bool);
-    }
-    //Prompt
-    openPrompt(){
-        const as = this.state.promptStatus;
-        switch (as){
-        case true:
-            this.setState({promptStatus:false});
-            break;
-        case false:
-            this.setState({promptStatus:true});
-            break;
-        }
-    }
-    closePrompt(bool,value){
-        this.setState({promptStatus:false});
-        if(bool){
-            alert(value);
-        }
-    }
-    //Dilog
-    openDilog(){
-        const as = this.state.dilogStatus;
-        switch (as){
-        case true:
-            this.setState({dilogStatus:false});
-            break;
-        case false:
-            this.setState({dilogStatus:true});
-            break;
-        }
-    }
-    closeDilog(bool){
-        this.setState({dilogStatus:false});
+    closeModal(bool){
+        this.setState({status:false});
         console.log(bool);
     }
     render() {
@@ -84,25 +30,16 @@ export default class ModalTest extends Component {
             <div>
                 <h5>模态窗口</h5>
                 <div>
-                    <button type="button" className="btn btn-md btn-promise" onClick={this.openAlert.bind(this)}>Alert modal</button>
-                    <button type="button" className="btn btn-md btn-promise" onClick={this.openConfirm.bind(this)}>Confirm modal</button>
-                    <button type="button" className="btn btn-md btn-promise" onClick={this.openPrompt.bind(this)}>Prompt modal</button>
-                    <button type="button" className="btn btn-md btn-promise" onClick={this.openDilog.bind(this)}>Dilog modal</button>
+                    <button type="button" className="btn btn-md btn-promise" onClick={this.openModal.bind(this)}>open modal</button>
                 </div>
-                <ModalAlert title={'alert modal'} width={'360px'} status={this.state.alertStatus} close={this.closeAlert.bind(this)}>
+                <Modal title={'modal title'} width={'360px'} status={this.state.status} close={this.closeModal.bind(this)}>
                     <p>this is some context</p>
-                </ModalAlert>
-                <ModalConfirm title={'confirm modal'} width={'360px'} status={this.state.confirmStatus} close={this.closeConfirm.bind(this)}>
                     <p>this is some context</p>
-                </ModalConfirm>
-                <ModalPrompt title={'Prompt modal'} width={'360px'} status={this.state.promptStatus} close={this.closePrompt.bind(this)} />
-                <ModalDilog
-                    title={'Dilog modal'}
-                    width={'260px'}
-                    status={this.state.dilogStatus}
-                    close={this.closeDilog.bind(this)}
-                    message={'this is some message'}
-                />
+                    <p>this is some context</p>
+                    <p>this is some context</p>
+                    <p>this is some context</p>
+                    <p>this is some context</p>
+                </Modal>
             </div>
         );
     }
