991eda2225f605b19ffe45c742cf2a59a0ac90a7
qjwvtd <176156562@qq.com>
2018-09-30 12:47:56 +0800

    2018-08-31 制作日期插件...

--- web/public/radio.js
+++ web/public/radio.js
@@ -1,8 +1,7 @@
 /**
  * radio组件
- * list:[{key:0,value:'男'},{key:1,value:'女'},...]
- * size:['sm','md','lg'],默认'sm'
- * callback:选择后的回调，返回当前选择的对象object
+ * data:[{key:0,value:'男'},{key:1,value:'女'},...]
+ * onchange:选择后的回调，返回当前选择的对象object
  * 用法：<Radio list={this.state.list} callback={this.onRadioChange.bind(this)} />
  **/
 import React,{Component} from 'react';
@@ -13,29 +12,32 @@ export default class Radio extends Component {
             selectIndex:null
         };
     }
-
     componentDidMount() {
-        console.log(this.props.list);
+        const list = this.props.data;
+        for(let i=0;i<list.length;i++){
+            if(!list[i].key || !list[i].value){
+                console.log('radio组件参数错误,正确格式为:data:[{key:0,value:男},{key:1,value:女},...]');
+            }
+        }
     }
     onSelect(item,index){
         this.setState({
             selectIndex:index
         });
-        this.props.callback(item);
+        this.props.onchange(item);
     }
     render() {
-        const sizeClass = this.props.size ? this.props.size : 'sm';
         const selectIndex = this.state.selectIndex;
         return (
             <a className="ui-radio-group">
                 {
-                    this.props.list.map((item,index) => {
+                    this.props.data.map((item,index) => {
                         const isActiveClass = selectIndex == index ? "ui-radio-box active" : "ui-radio-box";
                         return (
-                            <span key={item.value} className={isActiveClass}>
-                                <b className={sizeClass} onClick={this.onSelect.bind(this,item,index)}></b>
-                                <label className={sizeClass}>{item.value}</label>
-                            </span>
+                            [
+                                <span key={item.value} className={isActiveClass} onClick={this.onSelect.bind(this,item,index)}><i></i></span>,
+                                <label key={index} onClick={this.onSelect.bind(this,item,index)}>{item.value}</label>
+                            ]
                         );
                     })
                 }
--- web/src/component/testRadio.js
+++ web/src/component/testRadio.js
@@ -19,11 +19,7 @@ export default class RadioTest extends Component {
     render() {
         return (
             <div>
-                小：<Radio list={this.state.list} callback={this.onRadioChange.bind(this)} />
-                <br/>
-                中：<Radio list={this.state.list} size={'md'} callback={this.onRadioChange.bind(this)} />
-                <br/>
-                大：<Radio list={this.state.list} size={'lg'} callback={this.onRadioChange.bind(this)} />
+                <Radio data={this.state.list} onchange={this.onRadioChange.bind(this)} />
             </div>
         );
     }
