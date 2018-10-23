6c6120b6f3a1f27ba981704c663c5580955cc7b7
qjwvtd <176156562@qq.com>
2018-10-23 17:08:21 +0800

    2018-10-08 update

--- web/public/progress.js
+++ web/public/progress.js
@@ -1,57 +1,136 @@
 /**
- * progress,进度条组件
+ * progress,进度条组件[三种]
+ * 圆形进度条div:
+ * @params progress真实进度，number[1-100]
+ * @params size,直径，number
+ * @params theme,进度条的颜色，string
+ * <Progress progress={(Math.random()*100).toFixed(0)} size={100} theme={'#1089ff'} />
  * 直线进度条ProgressNormal:
- * @params progress真实进度，number
+ * @params progress真实进度，number[1-100]
  * @params width,宽度，number/百分比值
  * @params size,高度大小，number
  * <ProgressNormal progress={(Math.random()*100).toFixed(0)} width={360} size={30} />
+ * 圆形进度条canvas:
+ * @params progress真实进度，number[1-100]
+ * @params size,直径，number
+ * @params theme,进度条的颜色，string
+ * <ProgressCanvas progress={(Math.random()*100).toFixed(0)} size={100} theme={'#1089ff'} />
  **/
 import React,{Component} from 'react';
+//圆形进度条(div)
+export class Progress extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            progress:0,
+            size:props.size || 120,
+            theme:props.theme || '#1089ff'
+        };
+    }
+
+    init(percentage) {
+        let p = Math.round(percentage * 100);
+        const deg = p * 3.6;
+        let __progress = null;
+        const theme = this.state.theme;
+        const size = this.state.size;
+        const leftRect = 'rect(0px, ' + size / 2 + 'px, auto, 0px)';
+        const rightRect = 'rect(0px, auto, auto, ' + size / 2 + 'px)';
+        const right = document.getElementById("right"),
+            left = document.getElementById("left"),
+            desc = document.getElementById("desc");
+        if (p > 100 || p < 0) {
+            p = 100;
+        }
+        ;
+        if (deg <= 180) {
+            right.style.cssText = 'background:'+theme+';transform:rotate(' + (deg - 180) + 'deg);clip:' + rightRect;
+            left.style.cssText = 'background:#ccc;clip:' + leftRect;
+        } else {
+            right.style.cssText = 'background:'+theme+';transform:none;clip:' + rightRect;
+            left.style.cssText = 'background:'+theme+';transform:rotate(' + (deg - 360) + 'deg);clip:' + leftRect;
+        }
+        if (desc.innerText) {
+            __progress = p + "%";
+        }
+        if (desc.textContent) {
+            __progress = p + "%";
+        }
+        this.setState({
+            progress:__progress
+        });
+    }
+
+    componentDidMount() {
+        this.init(this.props.progress);
+    }
+
+    render() {
+        const size = this.state.size;
+        return (
+            <div className="ui-propress-wrap" style={{width:size+'px',height:size+'px'}}>
+                <div className="ui-propress-right-part">
+                    <div className="ui-propress-right" id="right"></div>
+                    <div className="ui-propress-r-shadow"></div>
+                </div>
+                <div className="ui-propress-left-part">
+                    <div className="ui-propress-left" id="left"></div>
+                    <div className="ui-propress-l-shadow"></div>
+                </div>
+                <span className="ui-propress-text-desc" id="desc" style={{lineHeight:size+'px'}}>{this.state.progress}</span>
+            </div>
+        );
+    }
+}
 //直线进度条
 export class ProgressNormal extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            progress:0,//百分比，number，默认0
-            width:props.width + 'px' || '100%',//默认100%
-            size:props.size && props.size > 20 ? props.size : 20//默认最小20px
+            progress: 0,//百分比，number，默认0
+            width: props.width + 'px' || '100%',//默认100%
+            size: props.size && props.size > 20 ? props.size : 20//默认最小20px
         };
     }
+
     componentDidMount() {
         setTimeout(() => {
             this.setState({
-                progress:this.props.progress
+                progress: this.props.progress
             });
-        },0);
+        }, 0);
     }
 
     render() {
         const __progress = this.state.progress;
         const __width = this.state.width;
         const __height = this.state.size;
-        const bsize = __height*2+'px '+__height+'px';
+        const bsize = __height * 2 + 'px ' + __height + 'px';
         return (
             <div className="ui-progress-normal" style={{width:__width,height:__height+'px'}}>
-                <span style={{width:__progress+'%',lineHeight:__height+'px',backgroundSize:bsize}}>{__progress+'%'}</span>
+                <span
+                    style={{width:__progress+'%',lineHeight:__height+'px',backgroundSize:bsize}}>{__progress + '%'}</span>
             </div>
         );
     }
 }
-//圆形进度条
-export class ProgressCircle extends Component {
+//圆形进度条(canvas)
+export class ProgressCanvas extends Component {
     constructor(props) {
         super(props);
         this.progress = this.props.progress ? this.props.progress : 0;//百分比，number，默认0
         this.size = this.props.size ? this.props.size : 120;//默认120px大小
         this.theme = this.props.theme ? this.props.theme : '#1089ff';//默认#1089ff
     }
+
     componentDidMount() {
         const c1 = this.refs.c1;
         const c2 = this.refs.c2;
-        this.initProgressCircle(c1,c2,this.progress);
+        this.initProgressCircle(c1, c2, this.progress);
     }
+
     //绘制圆
-    initProgressCircle(c1,c2,percent){
+    initProgressCircle(c1, c2, percent) {
         const ctx_1 = c1.getContext('2d');
         const ctx_2 = c2.getContext('2d');
         const show = this.refs.cshow;
@@ -80,7 +159,8 @@ export class ProgressCircle extends Component {
             if (angle > (percent / 100 * 360)) {
                 percentAge = percent;
                 window.cancelAnimationFrame(timer);
-            };
+            }
+            ;
             ctx_2.stroke();
             ctx_2.closePath();
             ctx_2.save();
--- web/src/component/testProgress.js
+++ web/src/component/testProgress.js
@@ -2,7 +2,7 @@
  * 此处是注释
  **/
 import React,{Component} from 'react';
-import {ProgressCircle,ProgressNormal} from './../../public/progress';
+import {Progress,ProgressCanvas,ProgressNormal} from './../../public/progress';
 export default class ProgressTest extends Component {
     constructor(props) {
         super(props);
@@ -15,8 +15,11 @@ export default class ProgressTest extends Component {
         return (
             <div>
                 <h5>进度条</h5>
+                <Progress progress={(Math.random()*1).toFixed(5)} size={100} theme={'#1089ff'} />
+                <br/>
                 <ProgressNormal progress={(Math.random()*100).toFixed(0)} width={360} />
-                <ProgressCircle progress={(Math.random()*100).toFixed(0)} size={100} theme={'#1089ff'} />
+                <br/>
+                <ProgressCanvas progress={(Math.random()*100).toFixed(0)} size={100} theme={'#1089ff'} />
             </div>
         );
     }
