6fd0b2b4992628f7010dd7bed2fba508c00588d3
qjwvtd <176156562@qq.com>
2018-10-25 18:13:56 +0800

    2018-10-08 update

--- web/public/ajax-globalAxios.js
+++ web/public/ajax-globalAxios.js
@@ -1,11 +1,20 @@
 /**
  * 全局axios配置
  * "Authorization":tokens,
- * "Content-Type":"application/json"
+ * 【"Content-Type":"application/json"】--非简单请求
+ * 非简单请求需要额外处理：
+ * Access-Control-Allow-Origin；
+ * Access-Control-Allow-Methods：对客户端回应服务器支持的请求方法列表；
+ * Access-Control-Allow-Headers：对客户端回应服务器支持的Header；
+ * 【application/x-www-form-urlencoded】--简单请求
+ * 【multipart/form-data】--简单请求
+ * 【text/plain】--简单请求
+
  **/
 import axios from 'axios';
 axios.interceptors.request.use(
     (config) => {
+        config.headers['Content-Type'] = 'application/x-www-form-urlencoded';//改为简单请求
         // 这里token需要在登录时存储起来
         const token = sessionStorage.getItem("token");
         if (token) {
