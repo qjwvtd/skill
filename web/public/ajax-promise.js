068e1f338a27cece515699d73a83d17ab5bf5592
qjwvtd <176156562@qq.com>
2019-03-19 18:13:42 +0800

    modify promise

--- web/public/ajax-promise.js
+++ web/public/ajax-promise.js
@@ -1,43 +1,43 @@
 //原生promise-ajax
 export function ajax(params){
+    params = params || {};
+    if (!params.url) {
+        throw new Error('Necessary parameters are missing.'); //必要参数未填
+    }
+    const options = {
+        url: params.url || '',
+        type: (params.type || 'GET').toUpperCase(),
+        timeout: params.timeout || 5000,
+        dataType: params.dataType || 'json',
+        data: params.data || {}
+    };
+    //创建xhr对象
+    const xhr = new (self.XMLHttpRequest || ActiveXObject)("Microsoft.XMLHTTP");
+    if (!xhr) {
+        return false;
+    }
+    //格式化参数
+    const formatParams = function (json) {
+        const arr = [];
+        for (let i in json) {
+            arr.push(encodeURIComponent(i) + '=' + encodeURIComponent(json[i]));
+        }
+        return arr.join("&");
+    };
+    const fmtData = formatParams(options.data);
+    const token = sessionStorage.getItem('TOKEN');
     return new Promise(function(resole,reject){
-        params = params || {};
-        if (!params.url) {
-            throw new Error('Necessary parameters are missing.'); //必要参数未填
-        }
-        const options = {
-            url: params.url || '',
-            type: (params.type || 'GET').toUpperCase(),
-            timeout: params.timeout || 5000,
-            async: params.async || true,
-            dataType: params.dataType || 'json',
-            data: params.data || {}
-        };
-        //格式化参数
-        const formatParams = function (json) {
-            const arr = [];
-            for (let i in json) {
-                arr.push(encodeURIComponent(i) + '=' + encodeURIComponent(json[i]));
-            }
-            return arr.join("&");
-        };
-        //创建xhr对象
-        const xhr = new (self.XMLHttpRequest || ActiveXObject)("Microsoft.XMLHTTP");
-        if (!xhr) {
-            return false;
-        }
-        //发送请求
-        const fmtData = formatParams(options.data);
+        //发送请求,可根据需要自定义请求头
         if(options.type == 'GET'){
-            xhr.open(options.type, options.url + '?' + fmtData, options.async);
-            // xhr.setRequestHeader('Authorization', 'Bearer ' + sessionStorage.getItem('TOKEN'));
+            xhr.open(options.type, options.url + '?' + fmtData, true);
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token);
             xhr.send(null);
         }
         if (options.type == 'POST' || 'PUT' || 'DELETE') {
-            xhr.open(options.type, options.url, options.async);
+            xhr.open(options.type, options.url, true);
             xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
             xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-            // xhr.setRequestHeader('Authorization', 'Bearer ' + sessionStorage.getItem('TOKEN'));
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token);
             xhr.send(fmtData);
         }
         //超时处理
