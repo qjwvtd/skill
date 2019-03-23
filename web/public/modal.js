d7e3dc78b72449a9565d8b916530471a7ab50374
qjwvtd <176156562@qq.com>
2019-03-23 17:04:12 +0800

    Update modal.js

--- web/public/modal.js
+++ web/public/modal.js
@@ -1,4 +1,4 @@
-/*
+/**
  * 基于react V16版本createPortal API的模态窗口
  * 需要modal.css
  * @params title--模态标题,string,默认'标题'
@@ -8,53 +8,28 @@
  * 例：<Modal width={'400px'} visible={false} onChange={this.closeModal.bind(this)}>
  *          <p>this is some modal content</p>
  *     </Modal>
- * */
-import React,{ Component } from 'react';
+ */
+import React from 'react';
 import {createPortal} from 'react-dom';
-class ModalHead extends Component{
-    constructor(props){
-        super(props);
-    }
-    close(){
-        this.props.close(false);
-    }
-    render(){
-        return (
+export default function Modal(props) {
+    const {title,width,visible,onChange,children} = props;
+    const body = window.document.body;
+    const bpr = visible ? '17px' : '';
+    const bof = visible ? 'hidden' : '';
+    body.style.cssText = 'padding-right:'+ bpr +';overflow:'+bof+';';
+    const modalNode = <div className="ui-modal">
+        <div className="ui-modal-body" style={{width:width || '600px'}}>
             <div className="ui-modal-head">
-                {this.props.title}
-                <span onClick={this.close.bind(this)}>×</span>
+                {title}<span onClick={() => onChange(false)}>×</span>
             </div>
-        );
-    }
-}
-export default class Modal extends Component{
-    constructor(props){
-        super(props);
-        this.body = window.document.body;
-        this.state = {
-            title:props.title || '标题',
-            width:props.width || '600px'
-        };
-    }
-    closeModal(bool){
-        this.props.onChange(bool);
-    }
-    render(){
-        const { children } = this.props;
-        this.body.style.paddingRight = this.props.visible ? '17px' : '';
-        this.body.style.overflow = this.props.visible ? 'hidden' : '';
-        this.node = <div className="ui-modal">
-            <div className="ui-modal-body" style={{width:this.state.width}}>
-                <ModalHead title={this.state.title} close={this.closeModal.bind(this,false)} />
-                <div className="ui-modal-content">
-                    { children }
-                </div>
-                <div className="ui-modal-foot">
-                    <span className="ui-modal-btn-no" onClick={this.closeModal.bind(this,false)}>取消</span>
-                    <span className="ui-modal-btn-ok" onClick={this.closeModal.bind(this,true)}>确定</span>
-                </div>
+            <div className="ui-modal-content">
+                { children }
             </div>
-        </div>;
-        return this.props.visible && createPortal(this.node,this.body);
-    }
+            <div className="ui-modal-foot">
+                <span className="ui-modal-btn-no" onClick={() => onChange(false)}>取消</span>
+                <span className="ui-modal-btn-ok" onClick={() => onChange(true)}>确定</span>
+            </div>
+        </div>
+    </div>;
+    return visible && createPortal(modalNode,body);
 }
