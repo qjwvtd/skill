3d07158242cca45b1c6d2880db04f5d1e563d6cc
qjwvtd <176156562@qq.com>
2019-09-16 13:34:30 +0800

    Update util.js

--- web/public/util.js
+++ web/public/util.js
@@ -203,4 +203,24 @@ export function splits(tranvalue) {
     }
     return arr;
 }
+/** 
+ * 获取指定的URL参数值 
+ * URL:http://www.quwan.com/index?name=tyler 
+ * 参数：paramName URL参数 
+ * 调用方法:getUrlParams("name") 
+ * 返回值:tyler 
+ */
+export default function getUrlParams(paramName) {
+  const str = window.location.hash
+  const xh = str.indexOf('?')
+  const query = str.substr(xh + 1, str.length)
+  const vars = query.split('&')
+  for (let i = 0; i < vars.length; i++) {
+    const pair = vars[i].split('=')
+    if (pair[0] === paramName) {
+      return pair[1]
+    }
+  }
+  return false
+}
 
