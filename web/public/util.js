424f32835e832725c935a9afa7ea97d61c67957b
qjwvtd <176156562@qq.com>
2020-01-21 15:50:33 +0800

    Update util.js

--- web/public/util.js
+++ web/public/util.js
@@ -214,25 +214,19 @@ export function splits(tranvalue) {
     }
     return arr;
 }
-/** 
- * 获取指定的URL参数值 
- * URL:http://www.quwan.com/index?name=tyler 
- * 参数：paramName URL参数 
- * 调用方法:getUrlParams("name") 
- * 返回值:tyler 
- */
-export default function getUrlParams(paramName) {
-  const str = window.location.hash;
-  const xh = str.indexOf('?');
-  const query = str.substr(xh + 1, str.length);
-  const vars = query.split('&');
-  for (let i = 0; i < vars.length; i++) {
-    const pair = vars[i].split('=');
-    if (pair[0] === paramName) {
-      return pair[1];
+//获取URL参数,返回一个对象
+export function getParam() {
+    const startIndex = window.location.href.indexOf('?');
+    const params = window.location.href.substr(startIndex, window.location.href.length);
+    const xh = params.indexOf('?');
+    const query = params.substr(xh + 1, params.length);
+    const list = query.split('&' || '&&');
+    const paramObj = {};
+    for (let i = 0; i < list.length; i++) {
+        const pair = list[i].split('=');
+        paramObj[pair[0]] = pair[1];
     }
-  }
-  return false;
+    return paramObj;
 }
 /**
  * 取出数组中随机几项元素
