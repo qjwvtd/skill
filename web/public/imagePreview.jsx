e73e4b7fb63a310349589d7fa762095433bddaa1
qjwvtd <176156562@qq.com>
2020-10-14 09:29:00 +0800

    Update imagePreview.jsx

--- web/public/imagePreview.jsx
+++ web/public/imagePreview.jsx
@@ -11,17 +11,7 @@ export function ImagePreviewer({ visible, setVisible, image, list }) {
     const [currentIndex, setCurrentIndex] = useState(null);
     const [currentImg, setCurrentImg] = useState(null);
     const [isActive, setIsActive] = useState(true);
-    useEffect(() => {
-        if (visible) {
-            const index = list.indexOf(image);
-            if (index === -1) {
-                console.log('不存在在默认图片');
-                return;
-            }
-            setCurrentIndex(index);
-            setCurrentImg(image);
-        }
-    }, [visible, image, list]);
+
     function prev() {
         if (currentIndex === 0) {
             console.log('已经是最前一张了');
@@ -30,29 +20,47 @@ export function ImagePreviewer({ visible, setVisible, image, list }) {
         if (currentIndex > 0) {
             const prevIndex = currentIndex - 1;
             setCurrentIndex(prevIndex);
-            setCurrentImg(list[prevIndex]);
+            setIsActive(true);
+            setTimeout(() => {
+                setCurrentImg(list[prevIndex]);
+                setIsActive(false);
+            }, 500);
         }
     }
     function next() {
-        if (currentIndex === list.length - 1) {
+        const size = list.length - 1;
+        if (currentIndex === size) {
             console.log('已经是最后一张了');
             return;
         }
-        if (currentIndex < list.length - 1) {
+        if (currentIndex < size) {
             const nextIndex = currentIndex + 1;
             setCurrentIndex(nextIndex);
-            setCurrentImg(list[nextIndex]);
+            setIsActive(true);
+            setTimeout(() => {
+                setCurrentImg(list[nextIndex]);
+                setIsActive(false);
+            }, 500);
         }
     }
     useEffect(() => {
         if (visible) {
+            const index = list.indexOf(image);
+            if (index === -1) {
+                console.log('没有默认图片');
+                return;
+            }
+            setCurrentIndex(index);
+            setCurrentImg(image);
             setIsActive(true);
             setTimeout(() => {
                 setIsActive(false);
-            }, 500);
+            }, 200);
         }
-
-    }, [currentIndex, visible]);
+    }, [visible, image, list]);
+    useEffect(() => {
+        return () => { clearTimeout(); };
+    }, []);
     return <Fragment>
         <div className="ui-img-previewer" style={{ display: visible ? 'block' : 'none' }}>
             <span className="ui-previewer-close" onClick={() => setVisible(false)}><svg viewBox="64 64 896 896" focusable="false" className="" data-icon="close" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M563.8 512l262.5-312.9c4.4-5.2.7-13.1-6.1-13.1h-79.8c-4.7 0-9.2 2.1-12.3 5.7L511.6 449.8 295.1 191.7c-3-3.6-7.5-5.7-12.3-5.7H203c-6.8 0-10.5 7.9-6.1 13.1L459.4 512 196.9 824.9A7.95 7.95 0 0 0 203 838h79.8c4.7 0 9.2-2.1 12.3-5.7l216.5-258.1 216.5 258.1c3 3.6 7.5 5.7 12.3 5.7h79.8c6.8 0 10.5-7.9 6.1-13.1L563.8 512z"></path></svg></span>
