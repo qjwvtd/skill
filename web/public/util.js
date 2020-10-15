171b5f5a506829739debb623758f910653c7e224
qjwvtd <176156562@qq.com>
2020-10-15 15:12:30 +0800

    Update util.js

--- web/public/util.js
+++ web/public/util.js
@@ -206,6 +206,34 @@ export function splits(tranvalue) {
     }
     return arr;
 }
+//获取URL参数终极大法,返回一个对象
+export function parseURL(url) {
+    const a = document.createElement('a');
+    a.href = url;
+    return {
+        source: url,
+        protocol: a.protocol.replace(':', ''),
+        host: a.hostname,
+        port: a.port,
+        query: a.search,
+        params: (function () {
+            let ret = {},
+                seg = a.search.replace(/^\?/, '').split('&'),
+                len = seg.length, i = 0, s;
+            for (; i < len; i++) {
+                if (!seg[i]) { continue; }
+                s = seg[i].split('=');
+                ret[s[0]] = s[1];
+            }
+            return ret;
+        })(),
+        file: (a.pathname.match(/\/([^/?#]+)$/i) || [''])[1],
+        hash: a.hash.replace('#', ''),
+        path: a.pathname.replace(/^([^/])/, '/$1'),
+        relative: (a.href.match(/tps?:\/\/[^/]+(.+)/) || [''])[1],
+        segments: a.pathname.replace(/^\//, '').split('/')
+    };
+}
 //获取URL参数,返回一个对象
 export function getParam() {
     const search = window.location.search;
